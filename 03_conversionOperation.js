8cf649b8ece6586fa33832495cb66a2650e0530d
Rojan Bhattarai <rojanbhattarai22@gmail.com>
2026-09-24 15:29:03 +0545

    good to see you!

---
+++ 03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// //"22" => 22
+// //"23456dfg" => NaN
+
+
+// let isLoddesIn = 1;
+// let boolean = Boolean(isLoddesIn);
+// console.log(boolean);
+//************************************************operation************************************** *//
+
+// let value = 3 
+// let negValue = -value
+// console.log(negValue);
+
+
+
+
+let str1 = "hello"
+
+let str2 ="rojan"
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
---
+++ datatype_summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types: string, Number, Boolearn, Null, Undefine,  Symbol, BigInt,.
+
+const score = 100;
+const scoree = false;
+ 
+// const score:Number = 100; //typescript
+ 
+const Tempeture = null;
+ 
+let temp;
+
+
+
+
+
+//Reference type ( Non Primitive)
+
+//Array, Objects, Functions
+
+ const heros = ["Rojan", "Ronaldo", "Ramos", "pepe"];
+ let myObj ={
+    name :" Rojan",
+    age: 20,
+ }
+
+ const myFunctions = function(){
+    console.log("heros");
+
+ }
+
+ console.log(typeof Tempeture);
+ console.log(heros);
+ console.log(myObj);
