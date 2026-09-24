a7c19e1ae9a61a3f20155f0c087cde611e6ee6b9
Rojan Bhattarai <rojanbhattarai22@gmail.com>
2026-09-24 16:38:47 +0545

    good to see  you again! thankyou boss.

--- 05_string.js
+++ 05_string.js
@@ -9,15 +9,15 @@
 
 
 let appName = new String('Gen-ZNest');
-console.log(appName);
+// console.log(appName);
 
-console.log(appName.length);
-console.log(appName.toUpperCase());
-console.log(appName.toLowerCase());
-console.log(appName.charAt(2));
-console.log(appName.charAt(3));
+// console.log(appName.length);
+// console.log(appName.toUpperCase());
+// console.log(appName.toLowerCase());
+// console.log(appName.charAt(2));
+// console.log(appName.charAt(3));
 
- appName = appName.toLowerCase();
+//  appName = appName.toLowerCase();
 
-console.log(appName.indexOf('g'));
-console.log(appName.indexOf('z'));
+// console.log(appName.indexOf('g'));
+// console.log(appName.indexOf('z'));
