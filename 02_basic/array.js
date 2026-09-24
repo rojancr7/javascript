8ee488275c748b045323fddb24ed0e81b89f956e
Rojan Bhattarai <rojanbhattarai22@gmail.com>
2026-09-24 22:00:34 +0545

    good to see  you again! thankyou boss.

---
+++ 02_basic/array.js
@@ -0,0 +1,51 @@
+// Array
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(myArray);
+
+// console.log(myArray[0]);
+
+
+
+
+// Array methods
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+// myArray.unshift(5);
+
+myArray.shift(5);
+
+// console.log(myArray);
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(2));
+
+
+
+const newArry = myArray.join()
+// console.log(myArray);
+// console.log(typeof newArry);
+
+
+//slice, splice 
+
+// console.log("A", myArray);
+// const myn1 = myArray.slice(1,3);
+// console.log(myn1);
+// console.log("B", myArray);
+
+// const myn2 = myArray.splice(1,3);
+// console.log("C", myArray);
+// console.log(myn2);
+// console.log("D", myArray);
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++array 2++++++++++++++++++++++++++++*/
+
+const myHeros = ["Ronaldo", "bale", " Ramos", "Luka"];
+const movieHeros = ["Nani", "Kabir Singh", " jersey", "jannat"];
+myHeros.push(movieHeros);
+
+console.log(myHeros);
+console.log(movieHeros);
+
